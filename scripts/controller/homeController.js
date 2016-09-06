0cc52cd0625e7a4bfc16c4dd7dd5ab0ba2277c7c
meganrm <meganrm@gmail.com>
2016-09-06 15:24:35 -0700

    linter fixes

--- scripts/controller/homeController.js
+++ scripts/controller/homeController.js
@@ -8,28 +8,28 @@
   //   $('#about').hide();
   // };
 
-homeController.index = function(ctx, next) {
-  if(ctx.complaints.length) {
-    complaintsView.index(ctx.complaints);
-  }
-  else{
-    page('/');
+  homeController.index = function(ctx, next) {
+    if(ctx.complaints.length) {
+      complaintsView.index(ctx.complaints);
+    }
+    else{
+      page('/');
+    };
   };
-};
 
-homeController.loadAll = function(ctx, next) {
-  var complaintData = function(allComplaints) {
-    ctx.complaints = Complaint.allComplaints;
-    next();
+  homeController.loadAll = function(ctx, next) {
+    var complaintData = function(allComplaints) {
+      ctx.complaints = Complaint.allComplaints;
+      next();
+    };
+    if (Complaint.allComplaints.length) {
+      ctx.complaints = Complaint.allComplaints;
+      next();
+    }
+    else{
+      Complaint.updateData(complaintData);
+    }
   };
-  if (Complaint.allComplaints.length) {
-    ctx.complaints = Complaint.allComplaints;
-    next();
-  }
-  else{
-    Complaint.updateData(complaintData);
-  }
-};
 
 
   module.homeController = homeController;
