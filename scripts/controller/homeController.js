864af5a8fd70b83d30cff2c90552ab1d194b0184
meganrm <meganrm@gmail.com>
2016-09-06 14:53:07 -0700

    controllers on index

--- scripts/controller/homeController.js
+++ scripts/controller/homeController.js
@@ -3,10 +3,34 @@
 (function(module) {
   var homeController = {};
 
-  homeController.reveal = function() {
-    $('#home').show();
-    $('#about').hide();
+  // homeController.reveal = function() {
+  //   $('#home').show();
+  //   $('#about').hide();
+  // };
+
+homeController.index = function(ctx, next) {
+  if(ctx.complaints.length) {
+    complaintsView.index(ctx.complaints);
+  }
+  else{
+    page('/');
   };
+};
+
+homeController.loadAll = function(ctx, next) {
+  var complaintData = function(allComplaints) {
+    ctx.complaints = Complaint.allComplaints;
+    next();
+  };
+  if (Complaint.allComplaints.length) {
+    ctx.complaints = Complaint.allComplaints;
+    next();
+  }
+  else{
+    Complaint.updateData(complaintData);
+  }
+};
+
 
   module.homeController = homeController;
 })(window);
