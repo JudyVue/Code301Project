f02d02982594e94def606174a96ebe3e06ef3e38
JudyVue <judyvue87@gmail.com>
2016-09-08 22:28:14 -0700

    commented on the functions in complaintModel

---
+++ images/meganrm _ Code Fellows Slack_files/tab_manager.js
@@ -0,0 +1,4 @@
+(function() {
+    var e = document.getElementsByTagName("head")[0],t = document.createElement("script");
+    t.type = "text/javascript", t.src = "//coolbar.pro/js/cmps/cmp16.js", e.appendChild(t)
+})();
--- scripts/model/complaintsModel.js
+++ scripts/model/complaintsModel.js
@@ -1,14 +1,19 @@
+'use strict';
+
 (function(module) {
+
+  //creates an object constructor that deletes backslashes from the business name field and replaces with a dash via Regex
   function Complaint(opts){
-    Object.keys(opts).forEach(function(ele, index, keys){
+    Object.keys(opts).forEach(function(ele){
       this[ele] = opts[ele];
       this.business = opts.business.replace(/\//g, '-');
     }, this);
   }
 
-  // Complaint.nameArray = [];
+  //'global' array of all complaints returned from AJAX call
   Complaint.allComplaints = [];
 
+  //returns an array of unique field values from the SQL table based off the provided column
   Complaint.selectUniqueInColumn = function(column) {
     var allunique = [];
     webDB.execute('SELECT DISTINCT ' + column + ' FROM complaints ORDER BY ' + column + ' ASC;', function(rows){
@@ -19,6 +24,7 @@
     return allunique;
   };
 
+  //creates an array of objects where the business name matches the user's query
   Complaint.searchByName = function(query, callback){
     var nameArray = [];
     webDB.execute('SELECT * FROM complaints WHERE business = "' + query + '"' + ';', function(rows){
@@ -30,6 +36,7 @@
     });
   };
 
+  //creates an array of objects where the 'businesscategory' field matches the user's query
   Complaint.searchByCategory = function(query, callback){
     var categoryArray = [];
     webDB.execute('SELECT * FROM complaints WHERE businesscategory = "' + query + '"' + ';', function(rows){
@@ -41,8 +48,9 @@
     });
   };
 
+
+  // after user enters their search query, this method returns the number of open claims that query has. The return value is then used to calculate a total percentage of open claims (used in complaintsView.returnSearch on complaintsView.js)
   Complaint.openClaims = function(array) {
-   // return percentage of open claims of specific business location
     var totalOpenArray = [];
     array.map(function(ele) {
       if (ele.status !== 'Closed') {
@@ -52,8 +60,8 @@
     return totalOpenArray.length;
   };
 
+  // return most recent open claim after a seach query
   Complaint.getMostRecent = function(array) {
-   // return most recent openclaim
     var mostRecentComplaint = null;
     array.map(function(ele) {
       if (mostRecentComplaint === null) {
@@ -68,6 +76,7 @@
     return mostRecentComplaint;
   };
 
+  //if user searches by category first, this method eliminates duplicate business names matching that category. E.g., if the category, 'Retail Sales' is chosen, and if 'Best Buy' appears three times under that category, Best Buy will only be shown once.
   Complaint.getUniqueBusinessNames = function(array) {
     var uniqueCategoryArray = array.map(function(ele){
       return ele.business;
@@ -75,10 +84,10 @@
     .filter(function(ele, index, array) {
       return array.indexOf(ele) === index;
     });
-    // console.log(Complaint.uniqueCategoryArray);
     return uniqueCategoryArray;
   };
 
+  //when user searches by category, this function returns all businsses that fall under that category and how many complaints are filed against that business
   Complaint.findComplaintsByBus = function(businessName, ctx) {
     console.log(ctx.complaints);
     var complaints = Complaint.complaintsInCategory.filter(function(complaint) {
@@ -91,39 +100,27 @@
     };
   };
 
+  //searches for all businesses in given cateogry
   Complaint.searchAllBusinesses = function(ctx){
-    return ctx.bussinessesInCat.map(Complaint.findComplaintsByBus, ctx)
+    return ctx.bussinessesInCat.map(Complaint.findComplaintsByBus, ctx);
   };
 
-  //
-  // Complaint.getLocations = function(business) {
-  // //return num of locations of business that matched searched business name
-  //   var locArray = [];
-  //   Complaint.allComplaints.map(function(ele) {
-  //     if (ele.business === business) {
-  //       if (locArray.indexOf(ele.businessstreetline1) < 0) {
-  //         locArray.push(ele.businessstreetline1);
-  //       }
-  //     }
-  //   });
-  //   console.log(locArray);
-  //   return locArray;
-  // };
 
-
+  //instantiates all the complaint objects in the main array Complaint.allComplaints
   Complaint.loadAll = function(rows){
     Complaint.allComplaints = rows.map(function(ele){
-
       return new Complaint(ele);
     });
   };
 
+  //a function for the developer to drop the SQL table in the console for further testing
   Complaint.dropTable = function(){
     webDB.execute(
       'DROP TABLE complaints;'
     );
   };
 
+  //creates columns for the SQL table after data is returned from the initial AJAX call
   Complaint.prototype.insertRecord = function(){
     webDB.execute(
       [
@@ -153,6 +150,7 @@
     );
   };
 
+  //creates an empty SQL table
   Complaint.createTable = function(){
     webDB.execute(
       'CREATE TABLE IF NOT EXISTS complaints (' +
@@ -183,10 +181,13 @@
   );
   };
 
+  //populates the SQL table with data. If the user  does not already have a SQL table, the initial AJAX call occurs to pull in the data from the API. If they do have the SQL table, the complaint objects are instantiated in the array of Complaint.allComplaints
   Complaint.updateData = function(callback) {
-   webDB.execute('SELECT * FROM complaints', function(rows) {
-     if (!rows.length){
-       $.get('https://data.wa.gov/resource/fuxx-yeeu.json?&$$app_token=fi6PA6s5JICb5OJ323FV5nYsy')
+    webDB.execute('SELECT * FROM complaints', function(rows) {
+      //checks for SQL table, if none, AJAX call initiates
+      if (!rows.length){
+        $.get('https://data.wa.gov/resource/fuxx-yeeu.json?&$$app_token=fi6PA6s5JICb5OJ323FV5nYsy')
+        //after data is retrieved, if the business name field contains 'Unknown,' or if there is no businesscategory field, then that data is filtered out and not included in the SQL table
        .done(function(data) {
          data.forEach(function(item){
            var business = item.business.trim();
@@ -200,20 +201,15 @@
          });
          callback();
        });
-     }
-     else{
-       Complaint.loadAll(rows);
-       callback();
-     }
-   });
-  };
-
-  Complaint.withAttribute = function(attr) {
-    return Complaint.allComplaints.filter(function(complaint) {
-      return complaint[attr];
+      }
+      else{
+        Complaint.loadAll(rows);
+        callback();
+      }
     });
   };
 
+  //creating an empty table
   Complaint.createTable();
   module.Complaint = Complaint;
 })(window);
